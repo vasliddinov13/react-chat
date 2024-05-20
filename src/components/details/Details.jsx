db2640edb77f14fae23519da0b55bc82d4067f91
vasliddinov13 <nurmuhammad.dev13@gmail.com>
2024-05-20 22:51:57 +0500

    details design created

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -3,7 +3,49 @@ import './details.css';
 
 const Details = () => {
   return (
-    <div className='details'>Details</div>
+    <div className='details'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem, ipsum dolor.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privecy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://i.pinimg.com/736x/33/59/dd/3359dd4175ee66c2490c7c0c2ee54e46.jpg" alt="" />
+                <span>img name</span>
+              </div>
+              <img src="./download.png" alt="" className='icon' />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared files</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+        </div>
+        <button>Block user</button>
+      </div>
+    </div>
   )
 }
 
