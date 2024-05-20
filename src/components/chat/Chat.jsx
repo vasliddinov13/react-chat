5c891862a22c8ef5af4c592d8f496d0596752582
vasliddinov13 <nurmuhammad.dev13@gmail.com>
2024-05-20 19:22:10 +0500

    chat design created

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './chat.css';
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
+  const [open,setOpen] = useState(false);
+  const [text,setText] = useState("");
+  const handleEmoji = e => {
+    setText(prev=>prev+e.emoji);
+    setOpen(false);
+  }
+
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center">
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae harum a tempora aut vero, exercitationem atque omnis dicta expedita necessitatibus unde consequatur tenetur! In deleniti sequi aut aspernatur iusto cum?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae harum a tempora aut vero, exercitationem atque omnis dicta expedita necessitatibus unde consequatur tenetur! In deleniti sequi aut aspernatur iusto cum?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae harum a tempora aut vero, exercitationem atque omnis dicta expedita necessitatibus unde consequatur tenetur! In deleniti sequi aut aspernatur iusto cum?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae harum a tempora aut vero, exercitationem atque omnis dicta expedita necessitatibus unde consequatur tenetur! In deleniti sequi aut aspernatur iusto cum?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input 
+          type="text" 
+          className='input' 
+          placeholder='Type a message...'
+          value={text}
+          onChange={e=>setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" onClick={()=>setOpen(prev=>!prev)} />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+          </div>
+        </div>
+        <button className='sendButton'>Send</button>
+      </div>
+    </div>
   )
 }
 
